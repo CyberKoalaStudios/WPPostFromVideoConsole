1bea4a768f4a8b865749077ac940927ceb72d63c
Vladimir Kiselev <first3395@gmail.com>
2022-10-22 16:21:41 +0300

    remove redis

--- WPPostFromVideoConsole/Interfaces/IWordPress.cs
+++ WPPostFromVideoConsole/Interfaces/IWordPress.cs
@@ -13,6 +13,7 @@ public interface IWordPress
     public Task<Post?> CreateNewPost(Video? video, MediaItem mediaItem, PostPublishType postPublishType);
 
     // ReSharper disable once UnusedMember.Global
+    // ReSharper disable once UnusedMember.Global
     public Task<IEnumerable<Post>> GetPosts();
     public Task<Post> GetPostById(int id);
     public Task<string> GetMediaUrlById(int? id);
